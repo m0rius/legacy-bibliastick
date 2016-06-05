58f475e5daeedfda3c9c1bb1ed0379e0486ddf71
FM@lilascat <emmanuel@octopuce.fr>
2016-06-05 13:08:47 +0200

    [wip] Testing some stuff, in the main page

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -7,4 +7,11 @@ class HomeController extends \Picon\Lib\Controller{
     public function indexAction(){
 
     }
+
+    public function searchAction(){
+        $_stickers  =   new \Models\StickerModel();
+        $keyword    =   $_GET["q"];
+        $searchedStikers    =   $_stickers->searchStickers($keyword);
+        $this->set(array("stickers" => $searchedStikers));
+    }
 }
--- models/StickerModel.php
+++ models/StickerModel.php
@@ -4,5 +4,28 @@ namespace Models;
 
 class StickerModel extends \Picon\Lib\Model{
 
+    public function getAllStickers(){
+        return self::$db->query("select * from stickers;")
+                        ->fetchAll();
+    }
+
+    public function searchStickers($search, $type = "text"){
+        $query  =   self::$db->prepare("select * from stickers where title like '%?%';");
+        $query->execute(array($search));
+        return $query->fetchAll();
+
+    }
+
+    public function searchStickersPerCatgory($category){
+
+    }
+
+    public function searchStickersPerAuthor($author){
+
+    }
+    
+    public function searchStickersPerTitle($title){
+
+    }
 
 }
