acbd18934463017ac12c2f897f7d64aac46b313e
FM@lilascat <emmanuel@octopuce.fr>
2016-06-07 17:46:19 +0200

    [wip] User management page v1 is done

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,6 +4,13 @@ namespace Controllers;
 
 class UserController extends \Picon\Lib\Controller{
 
+    const   U_ADD   =   1;
+    const   U_DEL   =   2;
+    const   U_VAL   =   3;
+    const   U_UPD   =   4;
+    const   U_PAS   =   5;
+    const   U_ERR   =   -1; 
+
     public function pre_action(){
         if($this->route["action"]   ==  "login"){
             $this->security->disable();
@@ -53,6 +60,65 @@ class UserController extends \Picon\Lib\Controller{
     
     public function listeAdminAction(){
         $this->layout   =   "back";
-        
+        $error          =   "";
+        $_users         =   new \Models\UserModel();
+
+        if($this->route["method"]   ==  "POST"){
+            switch($this->computeAction($_POST)) {
+                case self::U_ADD:
+                    $_users->addUser($_POST["pseudo"], $_POST["mail"], $_POST["type"], $_POST["pass"]);
+                    break;
+                case self::U_DEL:
+                    $_users->deleteUser($_POST["id"]);
+                    break;
+                case self::U_VAL:
+                    $toValidate =   isset($_POST["validate"]) && $_POST["validate"] == "validate";
+                    $_users->validateUser($_POST["id"], $toValidate);
+                    break;
+                case self::U_PAS:
+                    $_users->updatePasswordUser($_POST["id"], $_POST["pass"]);
+                case self::U_UPD:
+                    $_users->updateUser($_POST["id"], $_POST["pseudo"], $_POST["mail"], $_POST["type"]);
+                    break;
+                case self::U_ERR:
+                    $error  =   "Bad inputs";
+                    break;
+                default:
+                    throw new HttpException(500);
+                    break;
+            }
+        }
+
+        $this->set(
+                array(
+                    "users"             =>  $_users->getAllValidatedUsers(), 
+                    "usersToValidate"   =>  $_users->getAllToValidateUsers(),
+                    "error"             =>  $error
+                )
+            );
+    }
+
+    private function computeAction($f){
+        if(isset($f["id"])){
+            if(isset($f["delete"]) && $f["delete"] == "delete"){
+                return self::U_DEL;
+            }
+            if(
+                isset($f["pseudo"]) && isset($f["mail"]) && isset($f["type"]) && isset($f["pass"]) && isset($f["cpass"]) 
+                && $f["pseudo"] && $f["mail"]  && $f["type"] 
+                && $f["pass"] == $f["cpass"]
+            ){
+                if($f["pass"]){
+                    return ($f["id"] == "0") ? self::U_ADD : self::U_PAS;
+                } 
+                if($f["id"] != "0"){
+                    return   self::U_UPD;
+                }
+            }
+            if(isset($f["mark"]) && $f["mark"]  ==  "toValidate") {
+                return  self::U_VAL;
+            }
+        }
+        return self::U_ERR;
     }
 }
--- models/UserModel.php
+++ models/UserModel.php
@@ -4,13 +4,49 @@ namespace Models;
 
 class UserModel extends \Picon\Lib\Model{
    
-    /** TODO : crypt thos passwords **/
+    /** TODO : crypt those passwords **/
     public function checkInfos($pseudo, $pass){
         $query  =   self::$db->prepare("select type from users where pseudo = ? && pass = ?;");
         $query->execute(array($pseudo, $pass));
         $type   =   $query->fetchColumn(0);
         $query->closeCursor();
         return $type;
+    }
+
+    public function getAllValidatedUsers(){
+        return self::$db->query("select * from users where validation = 1;")
+                        ->fetchAll();
+    }
+
+    public function getAllToValidateUsers(){
+        return self::$db->query("select * from users where validation = 3;")
+                        ->fetchAll();
+    }
+
+    public function deleteUser($id){
+        $query  =   self::$db->prepare("delete from users where id = ? ;");
+        return $query->execute(array($id));
+    }
+
+    public function addUser($pseudo, $mail, $type, $pass, $validation = 3){
+        $query  =   self::$db->prepare("insert into users(pseudo, mail, type, pass, validation) values (?, ?, ?, ?, ?)");
+        return $query->execute(array($pseudo, $mail, $type, $pass, $validation));
+    }
+
+    public function updateUser($id, $pseudo, $mail, $type){
+        $query  =   self::$db->prepare("update users set pseudo = ?, mail = ?, type = ? where id = ?;");
+        return $query->execute(array($pseudo, $mail, $type, $id));
+    }
+
+    public function updatePasswordUser($id, $pass){
+        $query  =   self::$db->prepare("update users set pass = ? where id = ?;");
+        return $query->execute(array($pass, $id));
+    }
+
+    public function validateUser($id, $toValidate){
+        $v  =   $toValidate ? 1 : 2;
+        $query  =   self::$db->prepare("update users set validation = ? where id = ? ;");
+        return $query->execute(array($v, $id));
     }
 
 }
