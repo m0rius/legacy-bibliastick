5d42668fa27fac92c85e5023c1b33a86749d53e7
FM@lilascat <emmanuel@octopuce.fr>
2016-06-08 23:26:04 +0200

    [WIP] Working on most of the contribution validation & view system

--- controllers/ContributionController.php
+++ controllers/ContributionController.php
@@ -10,16 +10,39 @@ class ContributionController extends \Picon\Lib\Controller{
             $this->security->check(2);
     }
         parent::pre_action();
-        $this->set(array("pseudo" =>  $_SESSION["user"]["pseudo"], "authLevel" =>  $this->security->getAuthLevel())); 
+        $this->set(array(
+                        "pseudo"    =>  $_SESSION["user"]["pseudo"], 
+                        "authLevel" =>  $this->security->getAuthLevel()
+                    )); 
     }
 
     // Auth level : 1
     public function listeAction(){
-
+        $_contribution  =   new \Models\ContributionModel();
+        $this->set(array(
+                        "listeContrib"  =>  $_contribution->getAll($_SESSION["user"]["id"])
+                    ));
     }
 
     // Auth level : 2
     public function listeAdminAction(){
+        $_contribution  =   new \Models\ContributionModel();
+        if($this->route["method"] == "POST"){
+            if(isset($_POST["id"])){
+                if(isset($_POST["delete"]) && $_POST["delete"]){
+                    $_contribution->delete($_POST["id"]);
+                } else if(isset($_POST["validate"]) && $_POST["validate"]){
+                    $_contribution->updateValidation($_POST["id"], 1);
+                } else {
+                    $_contribution->updateValidation($_POST["id"], 2);
+                }
+            } else {
+                $this->sendViewError("Bad inputs");
+            }
+        }
+        $this->set(array(
+                        "listeContrib"  =>  $_contribution->getAll()
+                    ));
 
     }
 
--- models/ContributionModel.php
+++ models/ContributionModel.php
@@ -9,4 +9,41 @@ class ContributionModel extends \Picon\Lib\Model{
         $query->execute(array($content, $idAuthor, $idInfo));
 
     }
+
+    public function getAll($idAuthor    =   0){
+        $toReturn   =   array();
+        $levels     =   array(
+            3   =>  "waiting",
+            1   =>  "validated",
+            2   =>  "refused"
+        );
+        foreach($levels as $num => $level){
+            $results    =   $this->getPerValidation($num, $idAuthor);
+            if($results){
+                $toReturn[$level]   =   $results;
+            }
+        }
+        return $toReturn;
+    }
+
+    public function getPerValidation($lvValidation, $idAuthor = 0){
+        $sql    =   "select c.id as id, s.title as title_sticker, s.id as id_sticker, c.content as contenu, c.creation as date, u.pseudo as pseudo_author, u.mail as mail_author from contributions as c join infos as i on c.id_info = i.id join stickers as s on s.id = i.id_sticker join users as u on c.id_author = u.id where c.validation = ? " . (($idAuthor) ? "&& c.id_author = ? " : "") . ";"; 
+        $args   =   array($lvValidation);
+        $idAuthor && $args[] =  $idAuthor;
+
+        $query  =   self::$db->prepare($sql);
+        $query->execute($args);
+        return $query->fetchAll();
+    }
+
+    public function delete($id){
+        $query  =   self::$db->prepare("delete from contributions where id = ?;");
+        return $query->execute(array($id));
+    }
+
+    public function updateValidation($id, $lvValidation){
+        $query  =   self::$db->prepare("update contributions set validation = ? where id = ?;");
+        $query->execute(array($lvValidation, $id));
+
+    }
 }
