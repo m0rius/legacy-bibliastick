a0e24453e799029af40fd41bc32a8e0c54ea6a7b
FM@lilascat <emmanuel@octopuce.fr>
2016-06-11 23:54:33 +0200

    [wip] Working on the categories

--- controllers/CategorieController.php
+++ controllers/CategorieController.php
@@ -16,11 +16,44 @@ class CategorieController extends \Picon\Lib\Controller{
 
     // Auth level : 1
     public function listeAction(){
-
+        $idUser         =   $_SESSION["user"]["id"];
+        $_categories    =   new \Models\CategoryModel();
+        if($this->route["method"] == "POST"){
+            if(isset($_POST["title"]) && $_POST["title"]){
+                $_categories->createNew($_POST["title"], (isset($_POST["parent"]) ? $_POST["parent"] : 0), $idUser);
+            }
+        }
+        $this->set(array(
+                        "listes"    =>  $_categories->getAll($idUser),
+                        "fulliste"  =>  $_categories->getFullList()
+                    )
+                );
     }
 
     // Auth level : 2
     public function listeAdminAction(){
+        $idUser         =   $_SESSION["user"]["id"];
+        $_categories    =   new \Models\CategoryModel();
+        if($this->route["method"] == "POST"){
+            if(isset($_POST["id"])){
+                if(isset($_POST["delete"]) && $_POST["delete"]){
+                    $_categories->delete($_POST["id"]);
+                } else if(isset($_POST["validation"])) {
+                    if($_POST["validation"] == "validate"){
+                        $_categories->updateValidation($_POST["id"], 1);
+                    } else if($_POST["validation"] == "refuse"){
+                        $_categories->updateValidation($_POST["id"], 2);
+                    }
+                }
+            } else {
+                $this->sendViewError("Bad inputs");
+            }
+        }
+        $this->set(array(
+                        "listes"    =>  $_categories->getAll(),
+                        "fulliste"  =>  $_categories->getFullList()
+                    )
+                );
 
     }
 
--- models/CategoryModel.php
+++ models/CategoryModel.php
@@ -4,4 +4,58 @@ namespace Models;
 
 class CategoryModel extends \Picon\Lib\Model{
 
+
+    public function createNew($name, $idParent, $idAuthor){
+        $query  =   self::$db->prepare("insert into available_categories (name, parent, creation, validation, id_author) values (?, ". ($idParent ? $idParent : "NULL") .", NOW(), 3, ?);");
+        $query->execute(array($name, $idAuthor));
+    }
+    public function getFullList(){
+        $query  =   self::$db->query("select id, name from available_categories where validation = 1;");
+        return $query->fetchAll();
+    }
+
+    public function getAll($idAuthor = 0){
+        $toReturn   =   array();
+        $levels     =   array(
+            1   =>  "validated",
+            2   =>  "refused",
+            3   =>  "waiting"
+        );
+        foreach($levels as $num => $level){
+            $results    =   $this->getCategoriesPerValidation($num, $idAuthor);
+            if($results){
+                $toReturn[$level]   =   $results;
+            }
+        }
+        return $toReturn;
+    }
+
+    public function getCategoriesPerValidation($lvValidation, $idAuthor = 0){
+        $sql    =   "select 
+                        c.id as id, c.name as name , cp.name as parent, c.creation as creation,
+                        u.pseudo as pseudo_author, u.mail as mail_author 
+                    from 
+                        available_categories as c
+                    join users as u on c.id_author = u.id 
+                    left join available_categories as cp on c.parent = cp.id
+                    where c.validation = ? " 
+                    . ($idAuthor ? "&& c.id_author = ? " : "") 
+                    . ";";
+        $args   =   array($lvValidation);
+        $idAuthor && $args[] = $idAuthor;
+        $query  =   self::$db->prepare($sql);
+        $query->execute($args);
+        return $query->fetchAll();
+    }
+
+    public function delete($id){
+        $query  =   self::$db->prepare("delete from available_categories where id = ?;");
+        return $query->execute(array($id));
+    }
+
+    public function updateValidation($id, $lvValidation){
+        $query  =   self::$db->prepare("update available_categories set validation = ? where id = ?;");
+        return $query->execute(array($lvValidation, $id));
+    }
+
 }
