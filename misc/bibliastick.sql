3dd91b42f8727a71113b57312fab4abd8f80791d
Benjamin Sonntag <benjamin@sonntag.fr>
2016-06-17 19:29:35 +0200

    [enh] fix the MySQL model to be compatible with MySQL and not only MariaDB, add UTF-8 charset by default

--- misc/bibliastick.sql
+++ misc/bibliastick.sql
@@ -1,4 +1,6 @@
+
 -- create database bibliastick;
+SET NAMES UTF8;
 
 -- Users infos
 -- Prevoir : anonyme, profile supprimé
@@ -10,7 +12,8 @@ create table users(
     pass varchar(255) not null,
     validation int(10) not null default 3, -- 1 validé, 2 refusé, 3 en attente de modération
     id_photo int(10) not null
-);
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 
 -- Stickers entities
 create table stickers(
@@ -18,9 +21,8 @@ create table stickers(
     title varchar(255) not null,
     creation datetime not null,
     validation int(10) not null default 3, -- 1 validé, 2 refusé, 3 en attente de modération
-    id_author int(10) not null,
-    foreign key (id_author) references users(id) on delete cascade
-);
+    id_author int(10) not null
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
 -- Pictures infos
@@ -32,10 +34,8 @@ create table pictures(
     validation int(10) not null default 3, -- 1 validé, 2 refusé, 3 en attente de modération
     creation datetime not null,
     id_author int(10) not null,
-    id_sticker int(10) not null, 
-    foreign key (id_sticker) references stickers(id) on delete cascade,
-    foreign key (id_author) references users(id) on delete cascade
-);
+    id_sticker int(10) not null
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
 -- Stickers texts
@@ -47,22 +47,20 @@ create table infos(
     modification datetime not null,
     id_author int(10) not null,
     id_sticker int(10),
-    id_picture int(10),
-    foreign key (id_sticker) references stickers(id) on delete cascade,
-    foreign key (id_author) references users(id) on delete cascade,
-    foreign key (id_picture) references pictures(id) on delete cascade
-);
+    id_picture int(10)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+
+-- User contributions (moderated)
 create table contributions(
     id int(10) primary key auto_increment,
     content text not null,
     validation int(10) not null default 3, -- 1 validé, 2 refusé, 3 en attente de modération
     creation datetime not null,
     id_author int(10) not null,
-    id_info int(10) not null,
-    foreign key (id_author) references users(id) on delete cascade,
-    foreign key (id_info) references infos(id) on delete cascade
-);
+    id_info int(10) not null
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 
 -- Stickers categories
 create table available_categories( 
@@ -71,10 +69,9 @@ create table available_categories(
     parent int(10) default null, 
     creation datetime not null,
     validation int(10) not null default 3, -- 1 validé, 2 refusé, 3 en attente de modération
-    id_author int(10) not null,
-    foreign key (parent) references available_categories(id) on delete cascade,
-    foreign key (id_author) references users(id) on delete cascade
-);
+    id_author int(10) not null
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 
 create table effective_categories(
     id int(10) primary key auto_increment,
@@ -82,27 +79,59 @@ create table effective_categories(
     id_sticker int(10) not null,
     validation int(10) not null default 3, -- 1 validé, 2 refusé, 3 en attente de modération
     creation datetime not null,
-    id_author int(10) not null,
-    foreign key (id_category) references available_categories(id) on delete cascade,
-    foreign key (id_sticker) references stickers(id) on delete cascade
-    foreign key (id_author) references users(id) on delete cascade
-);
-
+    id_author int(10) not null
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
 create table liste_stickers(
     id int(10) primary key auto_increment,
     id_user int(10) not null,
-    id_sticker int(10) not null,
-    foreign key (id_user) references users(id) on delete cascade,
-    foreign key (id_sticker) references stickers(id) on delete cascade
-);
+    id_sticker int(10) not null
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 
 create table messages(
     id int(10) primary key auto_increment,
     content text not null,
     id_sender int(10) not null, 
-    id_recipient int(10) not null,
-    foreign key (id_sender) references users(id) on delete cascade,
-    foreign key (id_sender) references users(id) on delete cascade
-);
+    id_recipient int(10) not null
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+alter table stickers 
+      add foreign key (id_author) references users(id) on delete cascade;
+
+alter table pictures
+      add foreign key (id_sticker) references stickers(id) on delete cascade,
+      add foreign key (id_author) references users(id) on delete cascade;
+
+alter table infos
+      add foreign key (id_sticker) references stickers(id) on delete cascade,
+      add foreign key (id_author) references users(id) on delete cascade,
+      add foreign key (id_picture) references pictures(id) on delete cascade;
+
+
+alter table contributions
+      add foreign key (id_author) references users(id) on delete cascade,
+      add foreign key (id_info) references infos(id) on delete cascade;
+
+
+alter table available_categories
+      add foreign key (parent) references available_categories(id) on delete cascade,
+      add foreign key (id_author) references users(id) on delete cascade;
+
+
+alter table effective_categories
+      add foreign key (id_category) references available_categories(id) on delete cascade,
+      add foreign key (id_sticker) references stickers(id) on delete cascade,
+      add foreign key (id_author) references users(id) on delete cascade;
+
+alter table liste_stickers
+      add foreign key (id_user) references users(id) on delete cascade,
+      add foreign key (id_sticker) references stickers(id) on delete cascade;
+
+alter table messages
+      add foreign key (id_sender) references users(id) on delete cascade,
+      add foreign key (id_sender) references users(id) on delete cascade;
+
+
